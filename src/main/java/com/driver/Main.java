f01d4894e98356fbd2e173eedd533378b82a503d
Moiz Khan <er.moiz2102@gmail.com>
2023-11-25 16:31:50 +0530

    code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[]args){
+        RWOnly obj= new RWOnly();
+//  obj.name="Moiz";
+//  System.out.println(obj.name);
+//    the name has private access modifier
+        obj.setName("moiz");
+        System.out.println(obj.getName());
+    }
+
 }
